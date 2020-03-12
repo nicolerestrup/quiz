4cec4ee3fa233bc36ee4067545a8077043931a38
nicolerestrup <anicolerestrup@gmail.com>
2020-03-12 10:11:40 +0100

    Added backend

---
+++ src/server.js
@@ -0,0 +1,28 @@
+// import { AllHtmlEntities as entities } from 'html-entities';
+const express = require('express');
+const path = require('path');
+const app = express();
+const axios = require('axios').default;
+const apiUrl = 'https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=boolean';
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/questions', async function (req, res) {
+  const result = await axios.get(apiUrl);
+  const questions = result.data.results.map((question, index) => {
+    return {
+      id: index,
+      correct_answer: question.correct_answer,
+      incorrect_answer: question.incorrect_answers,
+      question: question.question
+    }
+  });
+ return res.send(questions);
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
